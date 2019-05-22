4a85132f925dabb125b4d77076f1ae58b020276b
Miguelangel <silvermiguel96@gmail.com>
2019-05-22 14:52:36 -0500

    text & color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <h1>This is Home</h1>
+    <h1>Homepange</h1>
+    <p class="blue white--text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima nam eveniet magnam, explicabo, enim quasi aliquam, quis impedit magni vero iure consequuntur sint nisi quae exercitationem. Nemo sint quam ipsa.</p>
+    <p class="pink lighten-4 red-text text--darken-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, in officiis. Assumenda perferendis, consequatur magni reiciendis doloribus nam ex, cupiditate magnam vero obcaecati, dolor quibusdam earum harum voluptate asperiores pariatur?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="displat-1">Smaller Display</h4>
+    <p class="headline"> This is a Headline</p>
+    <p class="subheading font-weight-bold">This is a Subheadline</p>
+    <p class="caption"> This is a caption</p>
   </div>
 </template>
 
