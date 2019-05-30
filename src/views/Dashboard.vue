2cfe0079c5ba789f94d13481a290977934ac0beb
Miguelangel <silvermiguel96@gmail.com>
2019-05-30 15:36:25 -0500

    Dummy Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,30 +2,54 @@
   <div class="Dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-card flat class="pa-3">
-        <v-layout row wrap>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>The net Ninja</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due By</div>
-            <div>1st jan 2019</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{ project.status }}</div>
           </v-flex>
         </v-layout>
+        <v-divider></v-divider>
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new Website', person: 'The Net Ninja', due: '1st Jan 2019' ,status: 'completed'},
+        { title: 'Design a new Website', person: 'The Net Ninja', due: '1st Jan 2019' ,status: 'ongoing'},
+        { title: 'Design a new Website', person: 'The Net Ninja', due: '1st Jan 2019' ,status: 'overdue'},
+        { title: 'Design a new Website', person: 'The Net Ninja', due: '1st Jan 2019' ,status: 'ongoing'}
+      ]
+    }
+  }
+};
 </script>
+<style>
+.project.completed {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato
+  ;
+}
+</style>
