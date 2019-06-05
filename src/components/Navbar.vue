0939acf084df18773e36fe047b3ce359fcaf2210
Miguelangel <silvermiguel96@gmail.com>
2019-06-05 12:35:04 -0500

    Avatars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,6 +13,16 @@
     </v-btn>
   </v-toolbar>
   <v-navigation-drawer v-model="drawer" app class="primary">
+    <v-layout column align-center>
+      <v-flex class="mt-5">
+        <v-avatar size="100">
+          <img src="/avatar-1.png" alt="">
+        </v-avatar>
+        <p class="white--text subheading mt-1">
+          The Net Ninja
+        </p>
+      </v-flex>
+    </v-layout>
     <v-list>
       <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
         <v-list-tile-action>
