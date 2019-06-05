530bbea89ad7c4540b05a9c2e668cd1aa930b14f
Miguelangel <silvermiguel96@gmail.com>
2019-06-05 10:26:43 -0500

    Tooltips

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,21 @@
   <div class="Dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-
-      <v-layout row class="my-5">
-        <v-btn small flat color="grey" @click="sortBy('title')">
-          <v-icon left small>folder</v-icon>
-          <span class="caption text-lowercase">By Project Name</span>
-        </v-btn>
-        <v-btn small flat color="grey" @click="sortBy('person')">
-          <v-icon left small>person</v-icon>
-          <span class="caption text-lowercase">By person</span>
-        </v-btn>
+      <v-layout row class="my-3">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">By Project Name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">By person</span>
+          </v-btn>
+          <span>Sort projects by person</span>
+        </v-tooltip>
       </v-layout>
 
       <v-card flat v-for="project in projects" :key="project.title">
@@ -28,8 +33,8 @@
             <div class="caption grey--text">Due By</div>
             <div>{{ project.due }}</div>
           </v-flex>
-            <!-- <div class="caption grey--text">Status</div> -->
-            <!-- <div>{{ project.status }}</div> -->
+          <!-- <div class="caption grey--text">Status</div> -->
+          <!-- <div>{{ project.status }}</div> -->
           <v-flex xs2 sm4 md2>
             <div class="right">
               <v-chip small :class="`${project.status}`">{{ project.status }}</v-chip>
@@ -47,16 +52,36 @@ export default {
   data() {
     return {
       projects: [
-        { title: 'Design a new Website', person: 'He Net Ninja', due: '1st Jan 2019' ,status: 'completed'},
-        { title: 'New Website', person: 'Ge Net Ninja', due: '1st Jan 2019' ,status: 'ongoing'},
-        { title: 'A new Website', person: 'The Net Ninja', due: '1st Jan 2019' ,status: 'overdue'},
-        { title: 'Design a new Website', person: 'Net Ninja', due: '1st Jan 2019' ,status: 'ongoing'}
+        {
+          title: "Design a new Website",
+          person: "He Net Ninja",
+          due: "1st Jan 2019",
+          status: "completed"
+        },
+        {
+          title: "New Website",
+          person: "Ge Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing"
+        },
+        {
+          title: "A new Website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "overdue"
+        },
+        {
+          title: "Design a new Website",
+          person: "Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing"
+        }
       ]
-    }
+    };
   },
   methods: {
     sortBy(prop) {
-      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
     }
   }
 };
